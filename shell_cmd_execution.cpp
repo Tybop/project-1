782b285b16c169d455a7ea0864954562123b717b
Tyler Brown <tybrown@bb136-03.adit.mines.edu>
2016-09-28 15:52:46 -0600

    Working on getting the external commands to work.

--- shell_cmd_execution.cpp
+++ shell_cmd_execution.cpp
@@ -1,23 +1,35 @@
 /**
- * This file contains implementations of the functions that are responsible for
- * executing commands that are not builtins.
- *
- * Much of the code you write will probably be in this file. Add helper methods
- * as needed to keep your code clean. You'll lose points if you write a single
- * monolithic function!
- */
+* This file contains implementations of the functions that are responsible for
+* executing commands that are not builtins.
+*
+* Much of the code you write will probably be in this file. Add helper methods
+* as needed to keep your code clean. You'll lose points if you write a single
+* monolithic function!
+*/
 
 #include "shell.h"
 #include <iostream>
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
 
 int Shell::execute_external_command(vector<string>& tokens) {
-  //TODO: implement
-  
-  
-  
-  cout << "External command called" << endl; // delete when implemented
-  return 0;
+
+  int status;
+  pid_t pid = fork();
+  pid_t waited;
+
+  switch(pid){
+    case -1:
+      cout << "External call failed (fork failure).\n";
+      return 1;
+    case 0:
+      execlp(("/bin/"+tokens[0]).c_str(), tokens[0].c_str(),NULL);
+      waited = waitpid(pid, &status,WIFEXITED);
+      break;
+    default:
+      break;
+  }
 }
